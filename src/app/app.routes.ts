5590ba5f42373a2c92aade865d5af88ba6a2a7e5
yoshihiro okuda <yoshi.private.acc@gmail.com>
2023-07-29 08:22:18 +0900

    og & analytics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router'
 
-import { OthersComponent } from './pages/others/others.component'
+// import { OthersComponent } from './pages/others/others.component'
 import { TopComponent } from './pages/top/top.component'
 
 export const routes: Routes = [
-  { path: 'top', component: TopComponent },
-  { path: 'others', component: OthersComponent },
-  { path: '**', redirectTo: 'top' },
+  { path: '', component: TopComponent },
+  { path: '**', redirectTo: '' },
 ]

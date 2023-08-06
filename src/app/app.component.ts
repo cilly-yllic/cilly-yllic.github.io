9a60b76701490be2d45d3f91ed8b221555328d98
yoshihiro okuda <yoshi.private.acc@gmail.com>
2023-08-07 05:05:24 +0900

    remove meaningless files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 
-import { LayoutComponent } from './layout/layout.component'
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LayoutComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- src/app/pages/top/work/work.component.ts
+++ src/app/pages/top/work/work.component.ts
@@ -1,35 +1,77 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { MatGridListModule } from '@angular/material/grid-list'
+import { SafeHtml } from '@angular/platform-browser'
 
-const WORKS = [
+const TYPES = {
+  url: 'url',
+  html: 'html',
+} as const
+interface UrlText {
+  type: (typeof TYPES)[typeof TYPES.url]
+  value: string
+}
+
+interface HtmlText {
+  type: (typeof TYPES)[typeof TYPES.html]
+  value: SafeHtml | string
+}
+
+interface WorkItem {
+  href: string
+  backgroundImageUrl: string
+  text: (UrlText | HtmlText)[]
+}
+
+interface Work {
+  title: string
+  items: WorkItem[]
+}
+
+const WORKS: Work[] = [
   {
-    url: 'https://mooodone.com',
-    imageUrl: 'assets/images/ogp/mooodone/main.png',
-    thumbnailUrl: 'assets/images/ogp/mooodone/main.png',
-    userIconUrl: '',
-    description: '代表を勤めている合同会社です。（まだ独りです）',
+    title: '',
+    items: [
+      {
+        href: 'https://mooodone.com/',
+        backgroundImageUrl: 'assets/images/ogp/mooodone/main.png',
+        text: [
+          { type: TYPES.url, value: 'https://mooodone.com/' },
+          { type: TYPES.html, value: '代表を勤めている合同会社です。<br>（まだ独りです）' },
+        ],
+      },
+    ],
   },
   {
-    url: 'https://github.com/cilly-yllic',
-    imageUrl: 'assets/images/ogp/github/main.png',
-    thumbnailUrl: 'assets/images/ogp/github/icon.png',
-    userIconUrl: 'assets/images/ogp/github/user-icon.jpeg',
-    description: '今は更新していませんが、正社員時代に少し投稿していました。',
+    title: 'OSS',
+    items: [
+      {
+        href: 'https://github.com/cilly-yllic',
+        backgroundImageUrl: 'assets/images/ogp/github/icon.png',
+        text: [{ type: TYPES.url, value: 'https://github.com/cilly-yllic' }],
+      },
+      {
+        href: 'https://www.npmjs.com/~cilly',
+        backgroundImageUrl: 'assets/images/ogp/npm/icon.png',
+        text: [
+          { type: TYPES.url, value: 'https://www.npmjs.com/~cilly' },
+          { type: TYPES.html, value: '自動化や効率化できそうなアイディアを公開しています。' },
+        ],
+      },
+    ],
   },
   {
-    url: 'https://www.npmjs.com/~cilly',
-    imageUrl: 'assets/images/ogp/npm/main.png',
-    thumbnailUrl: 'assets/images/ogp/npm/icon.png',
-    userIconUrl: 'assets/images/ogp/npm/user-icon.jpeg',
-    description: '自動化や効率化できそうなアイディアが浮かんだら公開したりしています。',
-  },
-  {
-    url: 'https://qiita.com/cilly',
-    imageUrl: 'assets/images/ogp/qiita/main.png',
-    thumbnailUrl: 'assets/images/ogp/qiita/icon.png',
-    userIconUrl: 'assets/images/ogp/qiita/user-icon.jpg',
-    description: '今は更新していませんが、正社員時代に少し投稿していました。',
+    title: 'NOTE',
+    items: [
+      {
+        href: 'https://qiita.com/cilly',
+        backgroundImageUrl: 'assets/images/ogp/qiita/icon.png',
+        text: [
+          { type: TYPES.url, value: 'https://qiita.com/cilly' },
+          { type: TYPES.html, value: '正社員時代に少し投稿していました。' },
+        ],
+      },
+    ],
   },
 ]
 
